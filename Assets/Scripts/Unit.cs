feeb3edbfc1ee07054a0fc8c4dd1783267597b4f
Xaxupos <seweryn.pokutycki@gmail.com>
2019-08-21 03:31:20 +0200

    Baked map navigation

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour
+{
+
+    const string ANIMATOR_SPEED = "Speed";
+    const string ANIMATOR_ALIVE = "Alive";
+    const string ANIMATOR_SHOOTING = "Shooting";
+
+    public Transform target;
+
+    NavMeshAgent nav;
+    Animator animator;
+
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if(target)
+        {
+            nav.SetDestination(target.position);
+        }
+        Animate();
+    }
+
+
+    protected virtual void Animate()
+    {
+        var speedVector = nav.velocity;
+        speedVector.y = 0;
+        float speed = speedVector.magnitude;
+        animator.SetFloat(ANIMATOR_SPEED, speed);
+    }
+}
